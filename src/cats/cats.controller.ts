230db7af2c9919346afef8525e450525a3f6a5eb
mssoheil <mssoheil88@yahoo.com>
2020-03-15 10:58:50 +0330

    feat: redirecting

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res, Req, HttpCode, Header } from '@nestjs/common';
+import { Controller, Get, Res, Req, HttpCode, Header, Redirect } from '@nestjs/common';
 
 @Controller('ca*s')
 export class CatsController {
@@ -13,6 +13,7 @@ export class CatsController {
 	@HttpCode(205)
 	@Header("myHeader", "none")
 	@Header("myCustomHeader", "10")
+	@Redirect('https://google.com', 301)
 	findAllBlackCats(): string {
 		
 		return "hello black cats"
